3b747588f679be34c7ab8ee17920b85643f22b02
Leehogyu <59430391+JamongFriend@users.noreply.github.com>
2024-10-23 23:21:00 +0900

    feat: 0~9 사이의 무작위 숫자를 얻는 기능 구현

---
+++ src/main/java/racingcar/RandomNum.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+
+public class RandomNum {
+    public int GetNum() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
+
